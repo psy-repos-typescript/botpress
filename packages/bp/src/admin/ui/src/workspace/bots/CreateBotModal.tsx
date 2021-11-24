27267d631410825d706f52b70005e9b82be87517
Rob Gordon <rchristopher.gordon@gmail.com>
2021-11-24 16:29:19 -0500

    feat(admin): create bot cloud option

--- packages/bp/src/admin/ui/src/workspace/bots/CreateBotModal.tsx
+++ packages/bp/src/admin/ui/src/workspace/bots/CreateBotModal.tsx
@@ -1,6 +1,6 @@
 import { Button, Classes, Dialog, FormGroup, InputGroup, Intent, Callout } from '@blueprintjs/core'
 import { BotConfig, BotTemplate } from 'botpress/sdk'
-import { lang } from 'botpress/shared'
+import { Checkbox, lang } from 'botpress/shared'
 import _ from 'lodash'
 import ms from 'ms'
 import React, { Component } from 'react'
@@ -44,16 +44,20 @@ interface State {
 
   selectedTemplate?: BotTemplate
   selectedCategory?: SelectOption<string>
+  isCloudBot: boolean
+  bpCloudApiKey: string
 }
 
-const defaultState = {
+const defaultState: Omit<State, 'templates' | 'categories'> = {
   botId: '',
   botName: '',
   selectedCategory: undefined,
   selectedTemplate: undefined,
   error: undefined,
   isProcessing: false,
-  generateId: true
+  generateId: true,
+  isCloudBot: false,
+  bpCloudApiKey: ''
 }
 
 class CreateBotModal extends Component<Props, State> {
@@ -117,7 +121,9 @@ class CreateBotModal extends Component<Props, State> {
       id: this.state.botId,
       name: this.state.botName,
       template: _.pick(this.state.selectedTemplate, ['id', 'moduleId']),
-      category: this.state.selectedCategory && this.state.selectedCategory.value
+      category: this.state.selectedCategory && this.state.selectedCategory.value,
+      isCloudBot: this.state.isCloudBot,
+      bpCloudApiKey: this.state.bpCloudApiKey
     }
 
     try {
@@ -157,7 +163,7 @@ class CreateBotModal extends Component<Props, State> {
           <div className={Classes.DIALOG_BODY}>
             <FormGroup
               label={lang.tr('admin.workspace.bots.create.name')}
-              labelFor="bot-name"
+              labelFor="input-bot-name"
               labelInfo="*"
               helperText={lang.tr('admin.workspace.bots.create.nameHelper')}
             >
@@ -212,6 +218,41 @@ class CreateBotModal extends Component<Props, State> {
                   options={this.state.categories}
                   value={this.state.selectedCategory}
                   onChange={selectedCategory => this.setState({ selectedCategory: selectedCategory as any })}
+                />
+              </FormGroup>
+            )}
+            <FormGroup
+              label={lang.tr('admin.workspace.bots.create.cloud')}
+              labelFor="checkbox-bot-cloud"
+              helperText={lang.tr('admin.workspace.bots.create.cloudHelper')}
+            >
+              <Checkbox
+                id="checkbox-bot-cloud"
+                label={lang.tr('admin.workspace.bots.create.cloudCheckbox')}
+                checked={this.state.isCloudBot}
+                onChange={e =>
+                  this.setState({
+                    isCloudBot: e.target.checked
+                  })
+                }
+              />
+            </FormGroup>
+            {this.state.isCloudBot && (
+              <FormGroup
+                label={lang.tr('admin.workspace.bots.create.api')}
+                labelFor="bot-api-key"
+                helperText={lang.tr('admin.workspace.bots.create.apiHelper')}
+              >
+                <InputGroup
+                  id="bot-api-key"
+                  tabIndex={2}
+                  placeholder={lang.tr('admin.workspace.bots.create.apiPlaceholder')}
+                  value={this.state.bpCloudApiKey}
+                  onChange={e =>
+                    this.setState({
+                      bpCloudApiKey: e.target.value
+                    })
+                  }
                 />
               </FormGroup>
             )}
